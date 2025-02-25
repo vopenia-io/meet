4c2c7f61d5cc7b50fcf0ba0c224f743e309f164f
lebaudantoine <lebaud.antoine131@gmail.com>
2025-02-25 19:56:00 +0100

    🚸(frontend) make explicit why a page is not found
    Enhance the error message based on @spaccoud's feedback,
    explain to the user why a page is not found.

--- src/frontend/src/components/NotFoundScreen.tsx
+++ src/frontend/src/components/NotFoundScreen.tsx
@@ -1,12 +1,19 @@
 import { CenteredContent } from '@/layout/CenteredContent'
 import { Screen } from '@/layout/Screen'
+import { Text } from '@/primitives/Text'
 import { useTranslation } from 'react-i18next'
+import { Bold } from '@/primitives'
 
 export const NotFoundScreen = () => {
   const { t } = useTranslation()
   return (
     <Screen layout="centered">
-      <CenteredContent title={t('notFound.heading')} withBackButton />
+      <CenteredContent title={t('notFound.heading')} withBackButton>
+        <Text centered>
+          {t('notFound.body')}{' '}
+          <Bold>https://visio.numerique.gouv.fr/xxx-yyyy-zzz.</Bold>
+        </Text>
+      </CenteredContent>
     </Screen>
   )
 }
