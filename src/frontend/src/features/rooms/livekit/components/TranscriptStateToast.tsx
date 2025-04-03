8e18efbd7aa31af890d3ec45c91ba747a3a8ce57
lebaudantoine <lebaud.antoine131@gmail.com>
2025-04-03 17:10:41 +0200

    wip rework transcribing toast

--- src/frontend/src/features/rooms/livekit/components/TranscriptStateToast.tsx
+++ src/frontend/src/features/rooms/livekit/components/TranscriptStateToast.tsx
@@ -1,14 +1,13 @@
 import { css } from '@/styled-system/css'
-import { RiRecordCircleLine } from '@remixicon/react'
 import { Text } from '@/primitives'
 import { useTranslation } from 'react-i18next'
 import { useRoomContext } from '@livekit/components-react'
+import { Spinner } from '@/primitives/Spinner'
 
-export const RecordingStateToast = () => {
+export const TranscriptStateToast = () => {
   const { t } = useTranslation('rooms', { keyPrefix: 'recording' })
 
   const room = useRoomContext()
-
   if (!room?.isRecording) return
 
   return (
@@ -19,25 +18,24 @@ export const RecordingStateToast = () => {
         top: '10px',
         left: '10px',
         paddingY: '0.25rem',
-        paddingX: '0.25rem 0.35rem',
-        backgroundColor: 'primaryDark.200',
-        borderColor: 'primaryDark.400',
+        paddingX: '0.75rem 0.75rem',
+        backgroundColor: 'primaryDark.100',
+        borderColor: 'white',
         border: '1px solid',
         color: 'white',
         borderRadius: '4px',
         gap: '0.5rem',
       })}
     >
-      <RiRecordCircleLine
-        size={20}
+      <Spinner size={20} variant="black" />
+      <Text
+        variant={'sm'}
         className={css({
-          color: 'white',
-          backgroundColor: 'danger.700',
-          padding: '3px',
-          borderRadius: '3px',
+          fontWeight: '500 !important',
         })}
-      />
-      <Text variant={'sm'}>{t('label')}</Text>
+      >
+        {t('transcript')}
+      </Text>
     </div>
   )
 }
