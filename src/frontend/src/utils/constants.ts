ae06873ff52d39189a2ec792bd868c7a7c56547d
lebaudantoine <lebaud.antoine131@gmail.com>
2025-04-04 15:53:46 +0200

    👔(frontend) update feedback form's URL
    Gregoire reworked our framework on user feedbacks.
    Update the Grist form's URL.

--- src/frontend/src/utils/constants.ts
+++ src/frontend/src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const GRIST_FEEDBACKS_FORM =
-  'https://grist.numerique.gouv.fr/o/docs/forms/1YrfNP1QSSy8p2gCxMFnSf/4' as const
+  'https://grist.numerique.gouv.fr/o/docs/cbMv4G7pLY3Z/USER-RESEARCH-or-LA-SUITE/f/26' as const
 
 export const BETA_USERS_FORM_URL =
   'https://grist.numerique.gouv.fr/o/docs/forms/3fFfvJoTBEQ6ZiMi8zsQwX/17' as const
