462c6c50e5a20b5c214997759a4f8e1d93cd22ba
lebaudantoine <lebaud.antoine131@gmail.com>
2025-04-29 16:22:45 +0200

    🔒️(backend) disable BrowsableAPIRenderer to prevent information leakage
    Remove BrowsableAPIRenderer from API options, restricting output to JSON
    format only. Prevents leakage of sensitive information like resource IDs and
    user identifiers that were previously exposed in renderer dropdown options.
    
    Issue identified in #YWH-PGM14336-4 report.
    These information was considered as a critical disclosure by hackers.

--- src/backend/meet/settings.py
+++ src/backend/meet/settings.py
@@ -263,6 +263,9 @@ class Base(Configuration):
             "rest_framework.parsers.JSONParser",
             "nested_multipart_parser.drf.DrfNestedParser",
         ],
+        "DEFAULT_RENDERER_CLASSES": [
+            "rest_framework.renderers.JSONRenderer",
+        ],
         "EXCEPTION_HANDLER": "core.api.exception_handler",
         "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
         "PAGE_SIZE": 20,
