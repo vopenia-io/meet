a4d1dd75ded61be6dcc5ab87266f17e6a651bc7c
Thomas Gourgues <thomas.gourgues@gmail.com>
2025-06-02 22:57:30 +0200

    Uses new text stream from livekit for segments

--- src/frontend/src/features/rooms/livekit/components/subtitles/Segments.tsx
+++ src/frontend/src/features/rooms/livekit/components/subtitles/Segments.tsx
@@ -3,40 +3,67 @@ import { layoutStore } from "@/stores/layout.ts";
 import { useSnapshot } from "valtio";
 import {useMaybeRoomContext} from "@livekit/components-react";
 import {useEffect, useState} from "react";
-import {Participant, RoomEvent, TrackPublication, TranscriptionSegment} from "livekit-client";
+import {Participant, RoomEvent, TextStreamReader, TrackPublication, TranscriptionSegment} from "livekit-client";
+import {ParticipantInfo} from "@livekit/protocol"
 
+interface Segment {
+  id: string,
+  text: string,
+  timestamp: number
+}
 
 export const Segments = () => {
 
   const room = useMaybeRoomContext()
-  const [transcriptions, setTranscriptions] = useState<{
-    [id: string]: TranscriptionSegment
-  }>({})
-
-
+  const [transcriptions, setTranscriptions] = useState<{[id: string]: Segment}>({})
 
   useEffect(() => {
     if (!room) {
       return
     }
 
-    const updateTranscriptions = (
-      segments: TranscriptionSegment[],
-      participant?: Participant,
-      publication?: TrackPublication
-    ) => {
+    // const updateTranscriptions = (
+    //   segments: TranscriptionSegment[],
+    //   participant?: Participant,
+    //   publication?: TrackPublication
+    // ) => {
+    //   setTranscriptions((prev) => {
+    //     const newTranscriptions = { ...prev }
+    //     for (const segment of segments) {
+    //       newTranscriptions[segment.id] = segment
+    //     }
+    //     return newTranscriptions
+    //   })
+    // }
+
+    const updateTranscriptions = async (reader: TextStreamReader, participantInfo: {identity: string}) => {
+      const message = await reader.readAll();
+      console.log(reader.info);
+      if (reader.info.attributes['lk.transcribed_track_id']) {
+        console.log(`New transcription from ${participantInfo.identity}: ${message}`);
+      } else {
+        console.log(`New message from ${participantInfo.identity}: ${message}`);
+      }
+
+      const newSegment: Segment = {
+        id: reader.info.attributes['lk.segment_id'],
+        text: message,
+        timestamp: reader.info.timestamp
+      }
+
       setTranscriptions((prev) => {
-        const newTranscriptions = { ...prev }
-        for (const segment of segments) {
-          newTranscriptions[segment.id] = segment
-        }
-        return newTranscriptions
-      })
+        const newTranscriptions = {...prev};
+        newTranscriptions[newSegment.id] = newSegment;
+
+        return newTranscriptions;
+      });
     }
 
-    room.on(RoomEvent.TranscriptionReceived, updateTranscriptions)
+    room.registerTextStreamHandler('lk.transcription', updateTranscriptions);
+
     return () => {
-      room.off(RoomEvent.TranscriptionReceived, updateTranscriptions)
+    //room.off(RoomEvent.TranscriptionReceived, updateTranscriptions)
+      room.unregisterTextStreamHandler("lk.transcription");
     }
   }, [room])
 
@@ -54,7 +81,7 @@ export const Segments = () => {
     })}>
       <ul>
         {Object.values(transcriptions)
-          .sort((a, b) => a.firstReceivedTime - b.firstReceivedTime)
+          .sort((a, b) => b.timestamp - a.timestamp)
           .map((segment) => (
             <li key={segment.id}>{segment.text}</li>
           ))}
