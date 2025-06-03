ee10fadbfe34e5238c8a5ea7be0b5b5ef00a2fdf
Thomas Gourgues <thomas.gourgues@gmail.com>
2025-06-03 14:59:45 +0200

    Added caption preference store

--- src/frontend/src/features/settings/components/tabs/CaptionsTab.tsx
+++ src/frontend/src/features/settings/components/tabs/CaptionsTab.tsx
@@ -2,6 +2,8 @@ import { Field, H } from '@/primitives'
 import { useTranslation } from 'react-i18next'
 import { useLanguageLabels } from '@/i18n/useLanguageLabels'
 import { TabPanel, TabPanelProps } from '@/primitives/Tabs'
+import { captionPreferenceStore } from '@/stores/captionPreference'
+import { useSnapshot } from 'valtio'
 
 export type CaptionsTabProps = Pick<TabPanelProps, 'id'>
 
@@ -9,6 +11,7 @@ export const CaptionsTab = ({ id }: CaptionsTabProps) => {
 
   const { t } = useTranslation('settings', { keyPrefix: 'captions' })
   const { languagesList, currentLanguage } = useLanguageLabels()
+  const preference = useSnapshot(captionPreferenceStore)
 
   return (
     <TabPanel padding={'md'} flex id={id}>
@@ -17,9 +20,10 @@ export const CaptionsTab = ({ id }: CaptionsTabProps) => {
         type="select"
         label={t('label')}
         items={languagesList}
-        defaultSelectedKey={currentLanguage.key}
+        defaultSelectedKey={preference.language}
         onSelectionChange={(lang) => {
         // TODO Set new language transcription
+          captionPreferenceStore.language = lang.toString()
         }}
       />
     </TabPanel>
