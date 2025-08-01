965d823d088d8b4b3e3872b219ce275d7a634513
lebaudantoine <lebaud.antoine131@gmail.com>
2025-08-01 12:35:42 +0200

    ✨(frontend) display position of a raised hand in the queue
    Users requested an enhanced visual indicator
    for raised hands on the participant tile.
    
    Most major video conferencing platforms display the position of a raised hand
    in the queue. This helps hosts quickly see who is requesting to speak,
    and in what order, without needing to open the full participant list.
    
    While a minor feature, this improvement is especially valuable for power user

--- src/frontend/src/features/rooms/livekit/hooks/useRaisedHand.ts
+++ src/frontend/src/features/rooms/livekit/hooks/useRaisedHand.ts
@@ -1,9 +1,39 @@
 import { LocalParticipant, Participant } from 'livekit-client'
-import { useParticipantAttribute } from '@livekit/components-react'
+import {
+  useParticipantAttribute,
+  useParticipants,
+} from '@livekit/components-react'
 import { isLocal } from '@/utils/livekit'
+import { useMemo } from 'react'
 
 type useRaisedHandProps = {
   participant: Participant
+}
+
+export function useRaisedHandPosition({ participant }: useRaisedHandProps) {
+  const { isHandRaised } = useRaisedHand({ participant })
+
+  const participants = useParticipants()
+
+  const positionInQueue = useMemo(() => {
+    if (!isHandRaised) return
+
+    return (
+      participants
+        .filter((p) => !!p.attributes.handRaisedAt)
+        .sort((a, b) => {
+          const dateA = new Date(a.attributes.handRaisedAt)
+          const dateB = new Date(b.attributes.handRaisedAt)
+          return dateA.getTime() - dateB.getTime()
+        })
+        .findIndex((p) => p.identity === participant.identity) + 1
+    )
+  }, [participants, participant, isHandRaised])
+
+  return {
+    positionInQueue,
+    firstInQueue: positionInQueue == 1,
+  }
 }
 
 export function useRaisedHand({ participant }: useRaisedHandProps) {
